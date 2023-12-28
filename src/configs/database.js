8a3030d543f5c1e47a4b8799b0a35758fecedbb5
Maeyy23 <msmabel23@gmail.com>
2023-12-28 18:03:35 +0100

    layouts,handlebars,routes setup

--- src/configs/database.js
+++ src/configs/database.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 
-const connectDB = async (mongo_uri) => {
+const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(mongo_uri, {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
--- src/index.js
+++ src/index.js
@@ -2,11 +2,17 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const rateLimit = require("express-rate-limit");
+const morgan = require("morgan");
+const exphbs = require("express-handlebars");
 
 // environment variables configurations
 const app = express();
 dotenv.config();
 const PORT = process.env.PORT || 3000;
+
+//Database Connection
+const connectDB = require("./configs/database");
+connectDB(process.env.MONGO_URI);
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
@@ -18,7 +24,19 @@ const limiter = rateLimit({
 // Routes
 app.use(limiter);
 app.use(express.json());
+app.use(
+  morgan("default", {
+    /*options */
+  })
+);
+app.use("/", require("./routes/index"));
 
+// Configure express-handlebars
+const hbs = exphbs.create({ defaultLayout: "main", extname: ".hbs" });
+app.engine(".hbs", hbs.engine);
+app.set("view engine", ".hbs");
+
+// Initializing server
 app.get("/", (req, res) => {
   res.status(200).json({
     message: "Server is Up and Running",
